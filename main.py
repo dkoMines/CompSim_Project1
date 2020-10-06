ce15a7c951c50cf6108374c499aecd574c122d37
dkoMines <dko@mymail.mines.edu>
2020-10-05 21:12:11 -0600

    Merged

--- main.py
+++ main.py
@@ -51,3 +51,52 @@ def alg(j):
         k += 1
     return t_max
 
+def createMatrix(fileName):
+	data = open(fileName)
+	largestNum = 0
+	numLines = 0
+	n = []
+	for lt in [line.split() for line in data]:
+		source = int(lt[0])
+		dest = int(lt[1])
+		weight = int(lt[2])
+		numLines += 1
+		if largestNum < dest:
+			largestNum = dest
+	for i in range(largestNum):
+		l = []
+		for j in range(numLines):
+			l.append(0)
+		n.append(l)
+	count = 0
+	data = open(fileName)
+	for lt in [line.split() for line in data]:
+		source = int(lt[0])
+		dest = int(lt[1])
+		weight = int(lt[2])
+		n[source][count] = 1*weight
+		n[dest][count] = -1*weight
+		count += 1
+	for i in n:
+		print(i)
+
+
+
+
+
+
+def write_output(filename, results):
+    with open(filename, 'w') as f:
+        for result in results:
+            string = 'OUTPUT\t:'
+            for arc in result[0]:
+                string += arc + ','
+            string = string[:-1]
+            string += '\t' + str(result[1])
+            f.write(f'{string:>10}\n')
+
+
+createMatrix("san-leemis79.net")
+N = [[]]    # rows=nodes, cols=arcs
+Y = [[]]
+Beta = make_Beta()
