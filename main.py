fe4896d0bfaa586d8f2b2b6f07e8dd195153197f
LukeJSD <ljulian@mymail.mines.edu>
2020-10-05 19:56:39 -0600

    l first commit

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import random
 import math
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 
+N = [[]]
+
+def T(j):
+    if None:
+        return 0
+    return T(j)
+
+def Y(i, j):
+    if None:
+        return 0
+    return Y(i, j)
+
+
+def alg():
+    j = None
+    k = 1
+    l = 0
+    t_max = 0.0
+    while l < abs(None):
+        if N[j][k] == -1:
+            i = 1
+            while N[i][k] != 1:
+                i += 1
+            t = T(i) + Y(i, j)
+            if t >= t_max:
+                t_max = t
+            l += 1
+        k += 1
+    return t_max
