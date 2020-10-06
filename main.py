4cf8a21b869845215987abdbe7d4d246e9e03834
LukeJSD <ljulian@mymail.mines.edu>
2020-10-05 22:22:13 -0600

    committing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 import math
 import sys
-#import numpy as np
-#import matplotlib.pyplot as plt
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
 def make_Beta():
@@ -30,19 +32,19 @@ def T(j):
     if len(Beta[j]) == 0:
         return 0
     total_time = 0
-    for node in Beta(j):
+    for node in Beta[j]:
         total_time += abs(N[node][j]) + T(node)
     return total_time
 
 
 def alg(j):
-    k = 1
+    k = 0
     l = 0
     t_max = 0.0
     while l < len(Beta[j]):
-        if N[j][k] == -1:
-            i = 1
-            while N[i][k] != 1:
+        if N[j][k] < 0:
+            i = 0
+            while N[i][k] < 1:
                 i += 1
             t = T(i) + Y[i][j]
             if t >= t_max:
@@ -51,38 +53,34 @@ def alg(j):
         k += 1
     return t_max
 
+
 def createMatrix(fileName):
-	data = open(fileName)
-	largestNum = 0
-	numLines = 0
-	n = []
-	for lt in [line.split() for line in data]:
-		source = int(lt[0])
-		dest = int(lt[1])
-		weight = int(lt[2])
-		numLines += 1
-		if largestNum < dest:
-			largestNum = dest
-	for i in range(largestNum):
-		l = []
-		for j in range(numLines):
-			l.append(0)
-		n.append(l)
-	count = 0
-	data = open(fileName)
-	for lt in [line.split() for line in data]:
-		source = int(lt[0])
-		dest = int(lt[1])
-		weight = int(lt[2])
-		n[source-1][count] = 1*weight
-		n[dest-1][count] = -1*weight
-		count += 1
-	for i in n:
-		print(i)
-
-
-
-
+    data = open(fileName)
+    largestNum = 0
+    numLines = 0
+    n = []
+    for lt in [line.split() for line in data]:
+        source = int(lt[0])
+        dest = int(lt[1])
+        weight = int(lt[2])
+        numLines += 1
+        if largestNum < dest:
+            largestNum = dest
+    for i in range(largestNum):
+        l = []
+        for j in range(numLines):
+            l.append(0)
+        n.append(l)
+    count = 0
+    data = open(fileName)
+    for lt in [line.split() for line in data]:
+        source = int(lt[0])
+        dest = int(lt[1])
+        weight = int(lt[2])
+        n[source - 1][count] = 1 * weight
+        n[dest - 1][count] = -1 * weight
+        count += 1
+    return n
 
 
 def write_output(filename, results):
@@ -91,12 +89,17 @@ def write_output(filename, results):
             string = 'OUTPUT\t:'
             for arc in result[0]:
                 string += arc + ','
-            string = string[:-1]
+            string = string[:-1] + ':'
             string += '\t' + str(result[1])
             f.write(f'{string:>10}\n')
 
 
-createMatrix("san-leemis79.net")
-N = [[]]    # rows=nodes, cols=arcs
+N = createMatrix("san-leemis79.net")
 Y = [[]]
 Beta = make_Beta()
+
+
+def main():
+    n = 30000
+    for i in range(n):
+        alg(5)
